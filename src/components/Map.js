b8990bea857b8832c63d0754f5d90f1aad932ff7
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-03-15 23:17:12 +0530

    Map working

--- src/components/Map.js
+++ src/components/Map.js
@@ -5,30 +5,49 @@ const Map = (props) => {
   
   useEffect(() => {
 
-    
+    // Geocoding for start location
+    const geocoder = new window.google.maps.Geocoder();
+    geocoder.geocode({ address: props.from }, (results, status) => {
+      if (status === 'OK' && results[0]) {
+        const startLat = results[0].geometry.location.lat();
+        const startLng = results[0].geometry.location.lng();
+        
+        // Geocoding for end location
+        geocoder.geocode({ address: props.to }, (results, status) => {
+          if (status === 'OK' && results[0]) {
+            const endLat = results[0].geometry.location.lat();
+            const endLng = results[0].geometry.location.lng();
 
-    const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: { lat: 25.37600873168248, lng: 82.95931146120571 },
-      zoom: 5,
-    });
+            const map = new window.google.maps.Map(document.getElementById('map'), {
+              center: { lat: startLat, lng: startLng },
+              zoom: 5,
+            });
 
-    const directionsService = new window.google.maps.DirectionsService();
-    const directionsRenderer = new window.google.maps.DirectionsRenderer({
-      map: map,
-    });
+            const directionsService = new window.google.maps.DirectionsService();
+            const directionsRenderer = new window.google.maps.DirectionsRenderer({
+              map: map,
+            });
 
-    const start = new window.google.maps.LatLng(25.37600873168248, 82.95931146120571);
-    const end = new window.google.maps.LatLng(25.25309105163956, 83.00257002485706);
+            const start = new window.google.maps.LatLng(startLat, startLng);
+            const end = new window.google.maps.LatLng(endLat, endLng);
 
-    const request = {
-      origin: start,
-      destination: end,
-      travelMode: 'DRIVING',
-    };
+            const request = {
+              origin: start,
+              destination: end,
+              travelMode: 'DRIVING',
+            };
 
-    directionsService.route(request, (result, status) => {
-      if (status === 'OK') {
-        directionsRenderer.setDirections(result);
+            directionsService.route(request, (result, status) => {
+              if (status === 'OK') {
+                directionsRenderer.setDirections(result);
+              }
+            });
+          } else {
+            console.error('Geocode was not successful for the end location:', status);
+          }
+        });
+      } else {
+        console.error('Geocode was not successful for the start location:', status);
       }
     });
   }, []);
