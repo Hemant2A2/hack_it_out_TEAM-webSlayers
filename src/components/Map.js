469f1f3cdc6fbf564e8a88cb529cf3f016c9ceac
Hemant Ayuj <hemant.ayuj@gmail.com>
2024-03-15 12:34:11 +0530

    static map on webpage

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,13 @@
-const { google } = require('googleapis');
-const polyline = require('google-polyline');
+import React from 'react'
 
-const apiKey = "";
+const Map = () => {
+  return (
+    <div>
+      <gmp-map center="25.31776237487793,82.9713363647461" zoom="14" map-id="DEMO_MAP_ID">
+      <gmp-advanced-marker position="25.31776237487793,82.9713363647461" title="My location"></gmp-advanced-marker>
+    </gmp-map>
+    </div>
+  )
+}
+
+export default Map
